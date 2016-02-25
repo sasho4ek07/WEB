191ef682d14198fbccd5e3e094044c27f4386d75
Alexander <sasho4ek07@mail.ru>
2016-02-25 17:06:07 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+import threading
 import socket
 s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
 s.bind (('', 2222))
 s.listen (11)
 while True:
+    threading._start_new_thread
     conn, addr = s.accept ()
     while True:
         data = conn.recv(1024)
