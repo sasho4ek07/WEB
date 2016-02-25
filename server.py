153d1dcc3c15708a97efafff1d757ea0d25bb179
Alexander <sasho4ek07@mail.ru>
2016-02-25 18:13:17 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 def accep ():
     s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
     s.bind (('', 2222))
-    s.listen (10)
+    s.listen ()
     while True:
         conn, addr = s.accept ()
         while True:
