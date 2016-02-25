3f092b67be9dab4c7e65111b365f088dca40eeaa
Alexander <sasho4ek07@mail.ru>
2016-02-25 16:26:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -2,10 +2,10 @@ import socket
 s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
 s.bind (('', 2222))
 s.listen (11)
-sock, addr = s.accept ()
+sock, addr = s.accept()
 while True:
 	data = sock.recv(1024)        
-        if data.decode ('utf-8') == 'close':
+        if data == 'close':
             break
         elif not data:
             break
