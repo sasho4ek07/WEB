f5261f2bb5415a6f57171199ee0fd6c79f66da19
Alexander <sasho4ek07@mail.ru>
2016-02-25 17:35:32 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,17 @@
-import threading
+import _thread
 import socket
 s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
 s.bind (('', 2222))
 s.listen (11)
-while True:
-    threading._start_new_thread
-    conn, addr = s.accept ()
+def accep ():
     while True:
-        data = conn.recv(1024)
-        if data.decode ('utf-8') == 'close':
-            conn.close ()
-            break
-        elif not data:
-            break
-        conn.send (data)
+        conn, addr = s.accept ()
+        while True:
+            data = conn.recv(1024)
+            if data.decode ('utf-8') == 'close':
+                conn.close ()
+                break
+            elif not data:
+                break
+            conn.send (data)
+_thread.start_new_thread(accep())
