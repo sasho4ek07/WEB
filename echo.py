31c0bb8f77a1cff0a96d6fd133f87d869efd2a5b
Alexander <sasho4ek07@mail.ru>
2016-02-26 15:07:25 +0300

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,19 +1,18 @@
-from twisted.internet.protocol import Factory, Protocol
-from twisted.internet import reactor
- 
-class Server(Protocol):
-  def connectionMade(self):
-    self.transport.write(self.factory.quote+'\r\n')
-  def connectionLost(self, reason):
-    print 'connection lost ...'
-  def dataReceived(self, data):
-    print data
-    self.transport.write(data)
-  
-class ServerFactory(Factory):
-  protocol = Server
-  def __init__(self, quote=None):
-    self.quote = quote 
-
-reactor.listenTCP(8007, ServerFactory("quote"))
-reactor.run()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('', 2222))
+s.listen(10)
+while True:
+    conn, addr = s.accept()
+    while True:
+            data = conn.recv(1024)
+            msg = data.decode('utf-8')
+            print(msg)
+            if msg == "close":
+                conn.close()
+                break
+            elif not data:
+                conn.close()
+                break
+            else:
+                conn.send(data)
+    conn.close()
