576f54eede391e1cf4fab52750b70b9705257233
Alexander <sasho4ek07@mail.ru>
2016-02-26 15:39:41 +0300

    Update echo.py

--- echo.py
+++ echo.py
@@ -1,25 +1,24 @@
 import socket
 import threading
 
+def accep():
+    while True:
+            data = conn.recv(1024)
+            msg = data.decode('utf-8')
+            print(msg)
+            if msg == "close":
+                conn.close()
+                break
+            elif not data:
+                conn.close()
+                break
+            else:
+                conn.send(data)
+    conn.close()
+    return
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 2222))
 s.listen(10)
-def accep():
-    while True:
-        conn, addr = s.accept()
-        while True:
-                data = conn.recv(1024)
-                msg = data.decode('utf-8')
-                print(msg)
-                if msg == "close":
-                    conn.close()
-                    break
-                elif not data:
-                    conn.close()
-                    break
-                else:
-                    conn.send(data)
-        conn.close()
-    return
 while True:
-    threading.Thread(accep())
+       conn, addr = s.accept()
+       threading.Thread(accep())
