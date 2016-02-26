f1c1b47b30437d0a8d515888a5cee208a4cd867e
Alexander <sasho4ek07@mail.ru>
2016-02-26 11:04:30 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 import threading
 import socket
+s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
+s.bind(('0.0.0.0', 2222))
+s.listen(100)
 def accep():
-    s = socket.socket (socket.AF_INET , socket.SOCK_STREAM)
-    s.bind(('0.0.0.0', 2222))
-    s.listen(10)
     conn, addr = s.accept()
     while True:
         data = conn.recv(1024)
@@ -12,8 +12,10 @@ def accep():
             conn.close()
             break
         elif not data:
+            conn.close()
             break
         conn.send(data)
-    pass
+    conn.close()
+    return
 while True:
     threading.Thread(accep())
