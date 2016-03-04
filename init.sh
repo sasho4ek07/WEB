f5985b1a5aac3b02e4a5ebfbdfd954471da516b8
Alexander <sasho4ek07@mail.ru>
2016-03-04 17:46:10 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
 #sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8000 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app
