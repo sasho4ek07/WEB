37e10f7e9063daf1ca73e9d6d1c504b96b72bb1c
Alexander <sasho4ek07@mail.ru>
2016-03-04 16:55:54 +0300

    Added files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+def app(environ, start_response):
+	status = '200 OK'
+	headers = [('Content-Type', 'text/plain')]	
+	strings = environ['QUERY_STRING'].split('&')
+    	body = [string+"\r\n" for string in strings]
+	start_response(status, headers)
+	return body
+
