c003da2057b07104b73890664db104cc3bd039cf
Alexander <sasho4ek07@mail.ru>
2016-03-09 10:39:49 +0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ﻿ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/nginx.conf
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 #sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
